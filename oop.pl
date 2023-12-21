7bf511de15478840a80b060077b5b80a502bdf80
filomar03 <unacreaturadivina@gmail.com>
2023-12-21 05:32:08 +0100

    Update oop.pl
    progetto di merda, sistemate classi e istanze

--- oop.pl
+++ oop.pl
@@ -1,10 +1,9 @@
 /*
 * TODO: 
-*   - which types are valid and how to handle their initialization
-*   - check 'form' (method body)
-*   - Si noti che le regole di unificazione Prolog si applicano a <value> ed al corpo del metodo (capire che vuol dire)
-*   - Il comportamento di make cambia a seconda di che cosa `e il primo argomento <instance-name> (capire che vuol dire)
-*   - convert class representation in binary predicates? pros and cons?  
+*   - disallow multiple fields or methods with same name
+*   - optimize with cuts
+*   - figure out third 'make' case (https://elearning.unimib.it/mod/forum/discuss.php?d=252585)
+*   - figure out which types are valid and how to handle their initialization
 *   - 
 */
 
@@ -13,8 +12,9 @@
 :- dynamic class/4.
 
 def_class(Cname, Parents, Parts) :-
-    \+ is_class(Cname),
-    atomic_list(Parents),
+    atom(Cname),
+    \+ class(Cname, _, _, _),
+    class_list(Parents),
     parse_class(Parts, Fields, Methods),
     assertz(class(Cname, Parents, Fields, Methods)).
 
@@ -24,7 +24,7 @@ def_class(Class, Parents) :-
 
 is_class(Cname) :-
     atom(Cname),
-    class(Cname, _, _).
+    class(Cname, _, _, _).
 
 
 %%% Class helpers
@@ -36,16 +36,19 @@ atomic_list([Head | Tail]) :-
     atomic_list(Tail).
 
 
-prova(Cls) :-
-    parse_class(Cls, X, Y),
-    writeln(X),
-    writeln(Y).
+class_list([]).
+
+class_list([Head | Tail]) :-
+    is_class(Head),
+    class_list(Tail).
 
 
+%%% disallow multiple fields or methods with same name
 parse_class([], [], []).
 
 parse_class([Part | Parts], [Part | Fields], Methods) :-
     functor(Part, field, 2),
+    !,
     arg(1, Part, Fname),
     atom(Fname),
     %%% add field type
@@ -53,6 +56,7 @@ parse_class([Part | Parts], [Part | Fields], Methods) :-
 
 parse_class([Part | Parts], [Part | Fields], Methods) :- 
     functor(Part, field, 3),
+    !,
     arg(1, Part, Fname),
     atom(Fname),
     %%% parse field type
@@ -64,7 +68,8 @@ parse_class([Part | Parts], Fields, [Part | Methods]) :-
     atom(Mname),
     arg(2, Part, Args),
     var_list(Args),
-    %%% check if body is callable
+    arg(3, Part, Body),
+    callable(Body),
     %%% patch 'this'
     parse_class(Parts, Fields, Methods).
 
@@ -76,34 +81,61 @@ var_list([Head | Tail]) :-
     var_list(Tail).
 
 
+%%% 'superclass([], _)' was created to allow 'is_instance(Inst)'
+superclass([], _).
+
+superclass(Super, Class) :-
+    is_class(Super),
+    class(Class, Parents, _, _),
+    member(Super, Parents).
+
+
 %%% Instance predicates
 
 :- dynamic instance/3.
 
 make(Iname, Cname, Fields) :-
+    atom(Iname),
+    !,
+    \+ instance(Iname, _, _),
     is_class(Cname),
+    %%%% initialize fields
     asserta(instance(Iname, Cname, Fields)).
+
+make(Iname, Cname, Fields) :-
+    var(Iname),
+    !,
+    is_class(Cname),
+    %%%% initialize fields
+    Iname = instance(_, Cname, Fields).
+
+%%% same as 2nd case ???
+make(Iname, Cname, Fields) :-
+    is_class(Cname),
+    %%%% initialize fields
+    Iname = instance(_, Cname, Fields).
 
 make(Iname, Cname) :-
     make(Iname, Cname, []).
 
 
-is_instance(Iname) :-
-    instance(Iname, _).
+is_instance(Inst, Parent) :-
+    functor(Inst, instance, 3),
+    arg(2, Inst, Cname),
+    is_class(Cname),
+    %%% check fields ???
+    superclass(Parent, Cname).
 
-is_instance(Iname, Parent) :-
-    atom(Parent),
-    instance(Iname, Cname),
-    class(Cname, Parents, _),
-    is_contained(Parent, Parents).
-    
+
+is_instance(Inst) :-
+    is_instance(Inst, []).
+
 
 inst(Iname, Inst) :-
-    atom(Iname).
+    atom(Iname),
+    var(Inst),
+    instance(Iname, Cname, Fields),
+    Inst =.. [instance, Iname, Cname, Fields]
 
 
 %%% Instance helpers
-
-is_contained(X, [X | _]).
-is_contained(X, [_ | Ys]) :-
-    is_contained(X, Ys).
