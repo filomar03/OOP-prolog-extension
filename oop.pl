b5f8128f551deaa1065be6d9150441c36dc4ac1c
filomar03 <unacreaturadivina@gmail.com>
2023-12-29 05:23:15 +0100

    completed instance creation with fields and inheritance

--- oop.pl
+++ oop.pl
@@ -1,34 +1,29 @@
-class(entity, [], [field(id, 0, integer)], []).
-class(person, [entity], [field(name, 'Mario', string)], []).
-class(robot, [entity], [field(manufacturer, 'Xiaomi', string), field(version, '0.1.2', string)], []).
-class(cyborg, [person, robot], [field(tflops, 43, integer)], []).
-
 /*
 * TODO: 
-*   - disallow multiple fields or methods with same name
-*   - optimize with cuts
-*   - figure out third 'make' case (https://elearning.unimib.it/mod/forum/discuss.php?d=252585)
-*   - figure out which types are valid and how to handle their initialization
-*   - single field/method per name maintain last ovveridden?
-*   - cache per instance complete fields?
-*   - 
+*   - implement 'fieldx'
+*   - check fields in 'is_isntance'
+*   - methods implementation
+*   - patch 'this' in methods body
+*   - refactor names
+*   - verify correctnes with tests
+*   - implement 3rd 'make' case
 */
 
 %%% --------------------------------------------------
 %%% Class predicates
 
-:- dynamic class/4.
-:- dynamic icache/2.
+:- dynamic class/4.     %%% represents the class object
+:- dynamic icache/2.    %%% represents fields that every isntance should have
 
 def_class(Cname, Parents, Parts) :-
     atom(Cname),
     \+ class(Cname, _, _, _),
     class_list(Parents),
     parse_class(Parts, Fields, Methods),
-    get_superfields(Parents, SuperFields),
-    append(Fields, SuperFields, InstFields),
-    list_to_set(InstFields, InstFieldsSet),
-    assertz(icache(Cname, InstFieldsSet)),
+    get_superfields(Parents, [], SuperFields),  %%% remove to disable instance fields caching
+    append(Fields, SuperFields, InstFields),    %%% remove to disable instance fields caching
+    list_to_set(InstFields, InstFieldsSet),     %%% remove to disable instance fields caching
+    assertz(icache(Cname, InstFieldsSet)),      %%% remove to disable instance fields caching
     assertz(class(Cname, Parents, Fields, Methods)).
 
 def_class(Class, Parents) :-
@@ -45,12 +40,10 @@ is_class(Cname) :-
 
 parse_class([], [], []).
 
-%%% invert order in which elements are parsed and transform to set
 parse_class([Part | Parts], [Part | Fields], Methods) :- 
     Part = field(Fname, Value, Type),
     !,
     atom(Fname),
-    atom(Type),
     call(Type, Value),
     parse_class(Parts, Fields, Methods).
 
@@ -67,78 +60,81 @@ parse_class([Part | Parts], Fields, [Part | Methods]) :-
     parse_class(Parts, Fields, Methods).
 
 
-get_superfields([], []).
-
-get_superfields(Parents, Fields) :-
-    get_superfields_aux(Parents, [], Fields).
-
-
-get_superfields_aux([Parent | Parents], StackFields, FieldsAcc) :-
+%%% currently using this predicate to retrieve superfields
+%%% because it's faster since it caches the result
+get_superfields([Parent | Parents], StackFields, FieldsAcc) :-
     icache(Parent, ParentFields),
     append(StackFields, ParentFields, NewStackFields),
-    get_superfields_aux(Parents, NewStackFields, FieldsAcc).
+    get_superfields(Parents, NewStackFields, FieldsAcc).
 
-get_superfields_aux([], FieldsAcc, FieldsSetAcc).
+get_superfields([], FieldsAcc, FieldsAcc).
 
 
-%%% unused
+get_superfields_nc([Parent | Parents], StackFields, FieldsAcc) :-
+    class(Parent, PParents, Fields, _),
+    append(Parents, PParents, NewStackParents),
+    append(StackFields, Fields, NewStackFields),
+    get_superfields_nc(NewStackParents, NewStackFields, FieldsAcc).
+
+get_superfields_nc([], Acc, Acc).
+
+
+/*
 full_parents(Class, ParentsAccSet) :-
     full_parents_aux([Class], [], ParentsAcc),
     list_to_set(ParentsAcc, ParentsAccSet).    
 
 
-%%% unused
 full_parents_aux([Parent | Parents], StackParentsAcc, ParentsAcc) :-
     class(Parent, PParents, _, _),
     append(Parents, PParents, NextStackParents),
     append(StackParentsAcc, PParents, NextStackParentsAcc),
     full_parents_aux(NextStackParents, NextStackParentsAcc, ParentsAcc).
 
-full_parents_aux([], StackParentsAcc, StackParentsAcc).
-
+full_parents_aux([], StackParentsAcc, StackParentsAcc). 
+*/
 
 superclass(Super, Class) :-
-    atom(Super),
-    atom(Class),
     class(Class, Parents, _, _),
     member(Super, Parents).
 
-%%% 'superclass([], _)' was created to allow 'is_instance(Inst)'
 superclass([], _).
 
 
 %%% --------------------------------------------------
 %%% Instance predicates
 
-:- dynamic instance/3.
+:- dynamic instance/3.  %%% represent an instance of a class
 
-make(Iname, Cname, _) :-
+make(Iname, Cname, Fields) :-
     atom(Iname),
     !,
     \+ instance(Iname, _, _),
-    is_class(Cname),
-    %%% initialize fields
-    asserta(instance(Iname, Cname, _)).
+    icache(Cname, ClassFields),     %%% swap with 'get_superfields_nc/3',
+                                    %%% then add current class fields and transform to set
+                                    %%% when instance fields caching is disabled
+    ifields_2_fields(Fields, ConvFields),
+    init_fields(ClassFields, ConvFields, InstFields),
+    asserta(instance(Iname, Cname, InstFields)).
 
-make(Inst, Cname, _) :-
+make(Inst, Cname, Fields) :-
     var(Inst),
     !,
-    is_class(Cname),
-    %%% initialize fields
-    Inst = instance(_, Cname, _).
+    icache(Cname, ClassFields),
+    ifields_2_fields(Fields, ConvFields),
+    init_fields(ClassFields, ConvFields, InstFields),
+    Inst = instance(_, Cname, InstFields).
 
-%%% same as 2nd case ???
-make(Inst, Cname, Fields) :-
-    is_class(Cname),
-    %%% initialize fields
-    Inst = instance(_, Cname, Fields).
+make(_, _, _) :-
+    %%% implement logic
+    true().
 
 make(Iname, Cname) :-
     make(Iname, Cname, []).
 
 
 is_instance(Inst, Parent) :-
-    Inst = instance(_, Cname, Fields),
+    Inst = instance(_, Cname, _),
     is_class(Cname),
     %%% check fields ???
     superclass(Parent, Cname).
@@ -154,24 +150,35 @@ inst(Iname, Inst) :-
     Inst = instance(Iname, Cname, Fields).
 
 
+field(Inst, Fname, Result) :-
+    is_instance(Inst),
+    atom(Fname),
+    Inst = instance(_, Cname, Fields),
+    member(field(Fname, Result, _), Fields).
+
+
 %%% --------------------------------------------------
 %%% instance helper predicates
 
-%%% unused
-gen_inst_fields(Cname, FieldsAcc) :-
-    full_parents(Cname, Parents),
-    gen_inst_fields_aux(Parents, [], Fields),
-    class(Cname, _, ClassFields, _),
-    append(ClassFields, Fields, FullFields),
-    list_to_set(FullFields, FieldsAcc).
+ifields_2_fields([InstField | InstFields], [field(FName, FValue, any) | Fields]) :-
+    arg(1, InstField, FName),
+    arg(2, InstField, FValue),
+    ifields_2_fields(InstFields, Fields).
+
+ifields_2_fields([], []).
 
 
-gen_inst_fields_aux([], Acc, Acc).
+init_fields([ClassField | ClassFields], Fields, [field(Fname, Fvalue, Ftype) | FieldList]) :-
+    field(Fname, _, Ftype) = ClassField,
+    member(field(Fname, Fvalue, _), Fields),
+    !,
+    call(Ftype, Fvalue),
+    init_fields(ClassFields, Fields, FieldList).
 
-gen_inst_fields_aux([Parent | Parents], StackFields, Acc) :-
-    class(Parent, _, ParentField, _),
-    append(StackFields, ParentField, NextStackFields),
-    gen_inst_fields_aux(Parents, NextStackFields, Acc).
+init_fields([ClassField | ClassFields], Fields, [ClassField | FieldList]) :-
+    init_fields(ClassFields, Fields, FieldList).
+
+init_fields([], _, []).
 
 
 %%% --------------------------------------------------
@@ -198,18 +205,26 @@ var_list([Head | Tail]) :-
     var_list(Tail).
 
 
-%%% implement a function to compare custom elements
-list_to_set(List, Set, EqFun) :-
-    list_to_set_aux(List, [], Set, EqFun).
+list_to_set(List, Set) :-
+    list_to_set_aux(List, [], Set).
 
 
-list_to_set_aux([], Set, Set, _).
+list_to_set_aux([], Set, Set).
 
-%%% implement a 'Fun' parameter to compare compound elements
-list_to_set_aux([Head | Tail], StackSet, Set, EqFun) :-
+list_to_set_aux([Head | Tail], StackSet, Set) :-
     member(Head, StackSet),
-    list_to_set_aux(Tail, StackSet, Set, EqFun).
+    list_to_set_aux(Tail, StackSet, Set).
 
-list_to_set_aux([Head | Tail], StackSet, Set, EqFun) :-
+list_to_set_aux([Head | Tail], StackSet, Set) :-
     append(StackSet, [Head], NewStackSet),
-    list_to_set_aux(Tail, NewStackSet, Set, EqFun).
+    list_to_set_aux(Tail, NewStackSet, Set).
+
+
+%%% Tests
+
+:- def_class(entity, [], [field(id, 0, integer)]).
+:- def_class(person, [entity], [field(name, "Mario", string)]).
+:- def_class(robot, [entity], [field(manufacturer, "Xiaomi", string), field(version, "0.0.1", string)]).
+:- def_class(cyborg, [person, robot], [field(tflops, 12, integer)]).
+
+:- make(iborg, cyborg, [id = 999, manufacturer = "Apple", tflops = 42]).
