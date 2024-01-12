80b8dc5867fb54c57bbe88acef85cd58cac10501
filomar03 <unacreaturadivina@gmail.com>
2024-01-12 03:42:53 +0100

    implemented nice type checking

--- README.md
+++ README.md
@@ -1,115 +1,174 @@
 # Basic Object-Oriented Programming (OOP) Extension for Prolog
 
-This Prolog extension introduces basic object-oriented programming concepts, allowing users to define classes, create instances, and work with fields and methods. Below are the main predicates exposed by this extension:
+This Prolog extension introduces basic object-oriented programming concepts, allowing users to define classes, create instances, and work with fields and methods
+
+Supported types for class fields are
+  - `any` (no type restriction),
+  - `atom`,
+  - `number`,
+  - `float` (subtype of number),
+  - `integer` (subtype of float),
+  - `list`,
+  - `string`,
+  - `classes` (with 'polymorphism').
+
+### Below are the main predicates exposed by this extension:
 
 **Class-related Predicates:**
 
 - `def_class/3`:
-  - Parameters: `Cname` (class name), `Parents` (list of parent classes), `Parts` (list of class parts - fields/methods).
-  - Usage: Defines a class with a unique name (`Cname`) having specified parent classes (`Parents`) and class parts (`Parts`), then asserts the class information into the database.
+  - Parameters: 
+    - `Cname` (atom).
+    - `Parents` (list of atoms).
+    - `Parts` (list of fields or methods): 
+      > field format: "field(`Fname` (*atom*), `Fvalue` (*any*), `Ftype` (*atom*))".
+      > method format: "method(`Mname` (*atom*), `Margs` (*list of variables*), `Mbody` (*callable*))".
+  - Usage: Defines a class named `Cname` with `Parents` as class parents and methods/fields from `Parts`, then asserts the class information into the database.
   - Note: 
     - The class name `Cname` must be unique.
-    - Fields with unspecified types are set to `atom`.
+    - Fields with unspecified types are set to `any` type.
     - Inheritance system traverse parents tree by depth.
-    - When fields/methods with same name/signature are defined multiple times in the same class, only the first occurrence will be considered.
-    - When inheriting a field, type must be the same or a subtype
+    - When fields/methods with same name/signature are defined multiple times in the same class, only the first occurrence will be accessible.
+    - When inheriting a field, type must be the same or a subtype.
 
 - `def_class/2`:
-  - Parameters: `Class` (class name), `Parents` (list of parent classes).
+  - Parameters: 
+    - `Class` (atom). 
+    - `Parents` (list of atoms).
   - Usage: A shorthand for defining a class without specifying any fields or methods.
 
 - `is_class/1`:
-  - Parameters: `Cname` (class name).
-  - Usage: Checks if the given argument is a defined class.
+  - Parameters: 
+    - `Cname` (atom).
+  - Usage: Checks if `Cname` is a defined class.
 
 **Instance-related Predicates:**
 
 - `make/3`:
-  - Parameters: `Iname` (instance name), `Cname` (class name), `Fields` (list of initial field values).
-  - Usage: Creates an instance of a class (`Cname`) with name (`Iname`), specified field values (`Fields`) and asserts the instance information into the database.
+  - Parameters: 
+    - `Iname` (atom).
+    - `Cname` (atom).
+    - `Fields` (list of initializers):
+      > initializer format: "`Fname` (*atom*) = `Fvalue` (*atom*)"
+  - Usage: Creates an instance of a class `Cname` named as `Iname` and fields initialized from `Fields` then asserts the instance information into the database.
   - Note: 
     - The instance name `Iname` must be unique.
     - When the same field is initialized multiple times only the first one will be considerated.
 
 - `make/3`:
-  - Parameters: `Inst` (variable), `Cname` (class name), `Fields` (list of initial field values).
-  - Usage: Creates an instance of a class (`Cname`) with specified field values (`Fields`) and binds it to the variable (`Inst`).
-
-- `make/3`:
-  - Parameters: `Inst` (instance), `Cname` (class name), `Fields` (list of initial field values).
-  - Usage: Creates an instance of a class (`Cname`) with specified field values (`Fields`) and checks if it the same as (`Inst`).
+  - Parameters: 
+    - `Inst` (variable).
+    - `Cname` (atom).
+    - `Fields` (list of initializers):  
+  - Usage: Creates an instance of a class `Cname` and fields initialized from `Fields` then binds it to `Inst`.
 
 - `make/2`:
-  - Parameters: `Iname` (instance name / variable / instance ), `Cname` (class name).
-  - Usage: A shorthand for calling `make/3` without specifying any initial fields.
+  - Parameters: 
+    - `Iname` (atom or variable).
+    - `Cname` (atom).
+  - Usage: A shorthand for creating instances without specifying initializers.
 
 - `is_instance/1`:
-  - Parameters: `Inst` (instance).
-  - Usage: Verifies that the given argument is a valid instance.
+  - Parameters:
+    - `Inst` (instance or atom).
+  - Usage: Verifies that `Inst` is a valid instance or a name for a valid instance.
+  - Notes:
+    - This predicate also accepts the name of an instance instead of only instance object, since it was not clear in the assignment.
 
 - `is_instance/2`:
-  - Parameters: `Inst` (instance), `Super` (class name).
-  - Usage: Verifies that the instance is a valid instance and a subclass of `Super`. 
+  - Parameters: 
+    - `Inst` (instance or atom).
+    - `Super` (name).
+  - Usage: Verifies that `Inst` is a valid instance or a name for a valid instance, then verifies that class named `Super` is a super-class (every class is a superclass of itself as specified [here](https://elearning.unimib.it/mod/forum/discuss.php?d=253566)) of `Inst` class. 
 
 - `inst/2`:
-  - Parameters: `Iname` (instance name), `Inst` (instance).
-  - Usage: Retrieves an instance with the given name from the database.
+  - Parameters: 
+    - `Iname` (atom).
+    - `Inst` (instance).
+  - Usage: Retrieves the instance named `Name` from the database.
 
 - `field/3`:
-  - Parameters: `Inst` (instance), `Fname` (field name), `Result` (variable).
-  - Usage: Gets the value of the specified field from the given instance and unifies it with `Result`.
-
-- `field/3`:
-  - Parameters: `Iname` (instance name), `Fname` (field name), `Result` (variable).
-  - Usage: Gets the value of the specified field from the instance with the given name and unifies it with `Result`.
+  - Parameters: 
+    - `Inst` (instance or atom).
+    - `Fname` (atom).
+    - `Result` (variable).
+  - Usage: Gets the value of the field named `Fname` from the given instance/instance named `Inst` and unifies it with `Result`.
 
 - `fieldx/3`:
-  - Parameters: `Inst` (instance), `Fnames` (list of field names), `Result` (resulting value).
-  - Usage: Gets a chain of fields from the given instance or instance name, unifying to `Result`.
+  - Parameters: 
+    - `Inst` (instance or atom).
+    - `Fnames` (list atoms).
+    - `Result` (variable).
+  - Usage: Gets the value of the first field specified in `Fnames` from the instance/instance named `Inst` and recursively uses it to extract the next field until the last one, which is then unified with `Result`.
 
-**The following predicates are Not Meant for End-User**
+---
+
+### ❗The following predicates are Not Meant for End-User ❗
 
 **Internal Class Helper Predicates:**
 
-- `parse_class/3`:
-  - Parameters: `Parts` (list of class parts), `Fields` (resulting variable), `Methods` (resulting variable).
-  - Usage: Parses the class parts into fields and methods, handling field types and dynamic method definitions.
+- `parse_class/4`:
+  - Parameters: 
+    - `Parents` (list of atoms).
+    - `Parts` (list of atoms).
+    - `Fields` (variable).
+    - `Methods` (variable).
+  - Usage: Splits into fields and methods, handle field types and dynamic methods creation
 
 - `check_subtype/2`:
-  - Parameters: `Field` (field), `Parents` (list of class names).
-  - Usage: checks that `Field` if inherited is a subtype of the inherited one.
+  - Parameters: 
+    - `Field` (field).
+    - `Parents` (list of atoms).
+  - Usage: Checks that the field `Field` if inherited is a subtype or the same type as the inherited one.
 
 - `get_superfields_nc/2`:
-  - Parameters: `Cname` (class name), `Acc` (variable).
-  - Usage: Retrieves all fields of a class (including inherited fields) and accumulates them.
+  - Parameters: 
+    - `Cname` (atom).
+    - `Acc` (variable).
+  - Usage: Retrieves the fields (own + inherited) of class `Cname` and binds them to `Acc`
 
 - `superclass/2`:
-  - Parameters: `Super` (class name), `Class` (class name).
-  - Usage: Checks if `Super` is a superclass of `Class`.
+  - Parameters: 
+    - `Super` (atom), 
+    - `Class` (atom).
+  - Usage: Checks whether the argument `Super` is a superclass of the argument `Class`.
+  - Notes:
+    - In this predicate a class is not considered a superclass of itself
 
 - `patch_body/3`:
-  - Parameters: `Body` (original body), `This` (variable), `PBody` (patched body).
-  - Usage: Patches the body of a function by replacing occurrences of 'this' with a variable (`This`).
-  - Note: `This` must be unified before going out of scope, for this function to be effective
+  - Parameters: -
+    - `Body` (callable), `This` (variable), `PBody` (variable).
+  - Usage: Patches the body of a function by replacing occurrences of `this` (atom) with the variable `This`.
+  - Note: `This` must be unified before going out of scope, for this function to be effective, basically this method doesn't allow to statically patch a method, instead it must be done at runtime. I implemented it this way because i thought it was cleaner since, in Prolog we don't really care about performance 
+
+- `get_method/2`:
+  - Parameters: 
+    - `Sign` (compound), 
+    - `Body` (variable).
+  - Usage: Retrieves the body of the method specified by the signature `Sign` and then binds it to `Body`.
+  - Notes:
+    - This predicate is meant to be called only by dynamic methods definitions, since `Sign` must contain a reference to the caller instance (an instance or instance name).
 
 **Internal Instance Helper Predicates:**
 
 - `ifields_2_fields/2`:
-  - Parameters: `IFields` (list of initializers), `Fields` (variable).
-  - Usage: Converts a list of initializers from 'make' to 'field' objects.
+  - Parameters: 
+    -`IFields` (list of initializers).
+    -`Fields` (variable).
+  - Usage: Converts a list of `initializers` to `fields` then binds them to `Field`.
 
 - `init_fields/3`:
-  - Parameters: `CFields` (list of fields), `Fields` (list of fields), `FieldList` (variable).
-  - Usage: Creates a list of class fields equal to `CFields`, but uses values provided by `Fields`.
-
-- `check_fields/2`:
-  - Parameters: `Fields` (list of fields), `CFields` (list of class fields).
-  - Usage: Checks that the 2 lists of fields (`Fields` and `CFields`) have the same fields (except from value).
+  - Parameters: 
+    - `CFields` (list of fields).
+    - `Fields` (list of fields).
+    - `FieldList` (variable).
+  - Usage: Creates a list of fields equal to `CFields`, but uses values provided by `Fields` then binds them to `FieldList`.
   
-- `type/2`:
-  - Parameters: `Value` (value), `Type` (type).
-  - Usage: check that `Value` is the same type as `Type`, if `Type` is allowed.
+- `check_fields/2`:
+  - Parameters: 
+    - `IFields` (list of fields).
+    - `CFields` (list of fields).
+  - Usage: Checks that the 2 lists of fields are identical.
+  - Notes: The order of the fields inside the list must also be the same, otherwise it will fail.
 
-- `subtype/2`:
-  - Parameters: `Subtype` (type), `Type` (type).
-  - Usage: check if `Subtype` is a subtype of `Type`.
+**If some predicates haven't been mentioned, it's because they are only needed to support those mentioned and are not intended to be used on their own, or they are utility which are not strictly related to this project**
--- oop.pl
+++ oop.pl
@@ -2,19 +2,12 @@
 %%%%
 
 :- dynamic class/4.     %%% represents the class object
-:- dynamic instance/3.  %%% represent an instance of a class
+:- dynamic instance/3.  %%% represent the instance object
 
-%%% --------------------------------------------------
+%%% -----------------------------------------------------------------
 %%% exposed class predicates
 
-%%% define a class object with a name, parents and fields/methods
-%%% - class name must be unique
-%%% - when the type of a field is not specified,
-%%%     it is automatically set to 'atom'
-%%% - parents are explored by depth, so the order of parents is important
-%%% - when fields/methods with same signature/name are defined,
-%%%     only the first occurrence will be considered
-%%% - when inheriting a field, type must be the same or a subtype
+%%% def_class/3: defines a class named `Cname` with `Parents` as class parents and methods/fields from `Parts`, then asserts the class information into the database.
 def_class(Cname, Parents, Parts) :-
     atom(Cname),
     \+ class(Cname, _, _, _),
@@ -26,21 +19,15 @@ def_class(Class, Parents) :-
     def_class(Class, Parents, []).
 
 
-%%% check if <Cname> is a class
+%%% is_class/2: checks if `Cname` is a defined class.
 is_class(Cname) :-
     atom(Cname),
     class(Cname, _, _, _).
 
-
-%%% --------------------------------------------------
+%%% -----------------------------------------------------------------
 %%% exposed instance predicates
 
-%%% create an instance of the class <Cname>, allow fields initialization
-%%%     and save resulting instance in database as <Iname>
-%%%     or binds it to variable <Inst>
-%%% - to assert an instance it's name must be unique
-%%% - when a field is initialized multiple times
-%%%     only the first one will be considerated
+%%% make/3: creates an instance of a class `Cname` named as `Iname` and fields initialized from `Fields` then asserts the instance information into the database.
 make(Iname, Cname, Fields) :-
     atom(Iname),
     !,
@@ -50,6 +37,7 @@ make(Iname, Cname, Fields) :-
     init_fields(ClassFields, ConvFields, InstFields),
     asserta(instance(Iname, Cname, InstFields)).
 
+%%% make/3: creates an instance of a class `Cname` and fields initialized from `Fields` then binds it to `Inst`.
 make(Inst, Cname, Fields) :-
     var(Inst),
     !,
@@ -64,26 +52,43 @@ make(Inst, Cname, Fields) :-
     init_fields(ClassFields, ConvFields, InstFields),
     Inst = instance(_, Cname, InstFields).
 
+
 make(Iname, Cname) :-
     make(Iname, Cname, []).
 
 
-%%% verify that <Inst> is a valid instance
+%%% is_instance/1 verifies that `Inst` is a valid instance or a name for a valid instance
 is_instance(Inst) :-
-    Inst = instance(_, Cname, Fields),
-    is_class(Cname),
+    instance(_, Cname, Fields) = Inst,
+    !,
     get_superfields_nc(Cname, ClassFields),
     check_fields(Fields, ClassFields).
 
-%%% verify that <Inst> is a valid instance
-%%% and check that <Super> is a superclass of <Inst>
+is_instance(Iname) :-
+    inst(Iname, Inst),
+    is_instance(Inst).
+
+
+%%% is_instance/2: verifies that `Inst` is a valid instance or a name for a valid instance, then verifies that class named `Super` is a super-class of `Inst` class. 
 is_instance(Inst, Super) :-
+    Inst = instance(_, _, _)
+    !,
     is_instance(Inst),
     instance(_, Cname, _) = Inst,
+    is_instance_aux(Super, Cname).
+
+is_instance(Iname, Super) :-
+    inst(Iname, Inst),
+    is_instance(Inst, Super).
+
+
+is_instance_aux(Super, Super) :- !.
+
+is_instance_aux(Super, Cname) :-
     superclass(Super, Cname).
 
 
-%%% retrieve instance with name <Iname> from database
+%%% inst/2: retrieves the instance named `Name` from the database.
 inst(Iname, Inst) :-
     atom(Iname),
     var(Inst),
@@ -91,22 +96,21 @@ inst(Iname, Inst) :-
     Inst = instance(Iname, Cname, Fields).
 
 
-%%% retrieve field with name <Fname> from instance <Inst>
+%%% field/3: gets the value of the field named `Fname` from the given instance `Inst` and unifies it with `Result`.
 field(Inst, Fname, Result) :-
-    is_instance(Inst),
+    instance(_, _, _) = Inst,
     !,
+    is_instance(Inst),
     atom(Fname),
-    Inst = instance(_, _, IFields),
-    contains(IFields, field(Fname, Result, _)).
+    instance(_, _, IFields) = Inst,
+    contains(field(Fname, Result, _), IFields).
 
-%%% retrieve field with name <Fname> from instance named <Iname>
 field(Iname, Fname, Result) :-
-    atom(Iname),
     inst(Iname, Inst),
     field(Inst, Fname, Result).
 
 
-%%% retrieve a chain of fields from instance or instance name <Inst>
+%%% fieldx/3: gets the value of the first field specified in `Fnames` from the instance / instance named `Inst` and recursively uses it to extract the next field until the last one, which is then unified with `Result`.
 fieldx(Inst, [Fname | Fnames], Result) :-
     field(Inst, Fname, TmpResult),
     fieldx(TmpResult, Fnames, Result).
@@ -117,26 +121,22 @@ fieldx(Result, [], Result).
 %%% --------------------------------------------------
 %%% internal class helper predicates
 
-%%% parse class fields/methods by dividing them to match 
-%%%     the class object layout
-%%% - check field types
-%%% - create dynamic methods definition (can be invoked with instance name
-%%%     or instance object itself) 
+%%% parse_class/4: splits into fields and methods, handle field types and dynamic methods creation
 parse_class(Parents, [Part | Parts], [Part | Fields], Methods) :- 
-    Part = field(Name, Value, Type),
+    field(Name, Value, Type) = Part,
     !,
     atom(Name),
-    type(Value, Type),
     check_subtype(Part, Parents),
+    is_type(Value, Type),
     parse_class(Parents, Parts, Fields, Methods).
 
 parse_class(Parents, [Part | Parts], Fields, Methods) :-
-    Part = field(Name, Value),
+    field(Name, Value) = Part,
     !,
     parse_class(Parents, [field(Name, Value, any) | Parts], Fields, Methods).
 
 parse_class(Parents, [Part | Parts], Fields, [Part | Methods]) :- 
-    Part = method(Name, Args, Body),
+    method(Name, Args, Body) = Part,
     atom(Name),
     var_list(Args),
     callable(Body),
@@ -150,7 +150,7 @@ parse_class(Parents, [Part | Parts], Fields, [Part | Methods]) :-
     parse_class(Parents, Parts, Fields, Methods).
 
 parse_class(Parents, [Part | Parts], Fields, [Part | Methods]) :- 
-    Part = method(Name, Args, Body),
+    method(Name, Args, Body) = Part,
     atom(Name),
     var_list(Args),
     callable(Body),
@@ -158,18 +158,20 @@ parse_class(Parents, [Part | Parts], Fields, [Part | Methods]) :-
 
 parse_class(_, [], [], []).
 
+%%% check_subtype/2: checks that the field `Field` if inherited is a subtype or the same type as the inherited one.
+check_subtype(_, []) :- !.
 
 check_subtype(field(Name, _, Type), Parents) :-
     get_superfields_nc_aux(Parents, [], SuperFields),
-    contains(SuperFields, field(Name, _, OldType)),
+    contains(field(Name, _, OldType), SuperFields),
     !,
     subtype(Type, OldType).
 
 check_subtype(_, _).
 
 
-%%% retrieves all fields of <Cname> (it's own plus
-%%%     all inherited ones from superclasses)
+%%% get_superfields_nc/2: retrieves the fields (own + inherited) 
+%%%     of class <Cname> and binds them to <Acc>
 get_superfields_nc(Cname, Acc) :-
     get_superfields_nc_aux([Cname], [], Acc).
 
@@ -182,7 +184,7 @@ get_superfields_nc_aux([Class | Parents], Fields, Acc) :-
 get_superfields_nc_aux([], Acc, Acc).
 
 
-%%% checks if <Super> is a superclass of <Class>
+%%% superclass/2: checks whether the argument `Super` is a superclass of the argument `Class`.
 superclass(Super, Class) :-
     class(Class, Parents, _, _),
     superclass_aux(Super, Parents).
@@ -196,12 +198,9 @@ superclass_aux(Super, [Parent | Parents]) :-
     superclass_aux(Super, NextParents).
 
 
-%%% patch body of a function by swapping atom 'this'
-%%%     with variable <This>
-%%% - 'this' is only patched when used in a callable predicates
-%%%     not in compounds
+%%% patch_body/3: patches the body of a function by replacing occurrences of `this` (atom) with the variable `This`.
 patch_body(Body, This, PBody) :-
-    Body = (Stmt, Stmts),
+    (Stmt, Stmts) = Body,
     Stmt =.. List,
     patch_stmt(List, This, PList),
     PStmt =.. PList,
@@ -217,7 +216,7 @@ patch_body(Stmt, This, PStmt) :-
 
 patch_stmt([Term | Terms], This, [This | PList]) :-
     atom(Term),
-    Term = this,
+    this = Term,
     !,
     patch_stmt(Terms, This, PList).
 
@@ -227,7 +226,7 @@ patch_stmt([Term | Terms], This, [Term | PList]) :-
 patch_stmt([], _, []).
 
 
-%%% retrieve the body of the method with matching signature
+%%% get_method/2: retrieves the body of the method specified by the signature `Sign` and then binds it to `Body`.
 get_method(Sign, Body) :-
     Sign =.. [_, Iname | _],
     atom(Iname),
@@ -267,21 +266,20 @@ match_msign([_ | Methods], Sign, Body) :-
 %%% --------------------------------------------------
 %%% internal instance helper predicates
 
-%%% convert list of initializers from 'make' to 'field' objects 
-ifields_2_fields([IField | IFields], [field(Name, Value, any) | Fields]) :-
-    arg(1, IField, Name),
-    arg(2, IField, Value),
+%%% ifields_2_fields/2: converts a list of `initializers` to `fields` then binds them to `Field`. 
+ifields_2_fields([Name = Value | IFields], [field(Name, Value, any) | Fields]) :-
     ifields_2_fields(IFields, Fields).
 
 ifields_2_fields([], []).
 
 
-%%% initialize instance fields with values 'make' (converted by 'ifields_2_fields')
-init_fields([CField | CFields], Fields, [field(Name, Value, Type) | FieldList]) :-
-    field(Name, _, Type) = CField,
-    contains(Fields, field(Name, Value, _)),
+%%% init_fields/3: creates a list of fields equal to `CFields`, but uses values provided by `Fields` then binds them to `FieldList`.
+init_fields(CFields, [], CFields) :- !.
+
+init_fields([field(Name, _, Type) | CFields], Fields, [field(Name, Value, Type) | FieldList]) :-
+    contains(field(Name, Value, _), Fields),
     !,
-    type(Value, Type),
+    is_type(Value, Type),
     init_fields(CFields, Fields, FieldList).
 
 init_fields([CField | CFields], Fields, [CField | FieldList]) :-
@@ -291,11 +289,11 @@ init_fields([], _, []).
 
 
 %%% check that fields are the same (except for value)
-check_fields([field(Name, Value, Type) | Fields], [field(Name, _, Type) | CFields]) :-
-    type(Value, Type),
-    check_fields(Fields, CFields).
+check_fields([field(Name, Value, Type) | IFields], [field(Name, _, Type) | CFields]) :-
+    is_type(Value, Type),
+    check_fields(IFields, CFields).
 
-check_fields([], []).
+check_fields([], _).
 
 
 %%% --------------------------------------------------
@@ -325,53 +323,73 @@ var_list([Head | Tail]) :-
     var_list(Tail).
 
 
-type(_, any) :- !.
+%%% defines the allowed types
+is_type(_, any) :- !.
 
-type(Value, atom) :-
+is_type(Value, atom) :-
     atom(Value),
     !.
 
-type(Value, number) :-
+is_type(Value, number) :-
     number(Value),
     !.
 
-type(Value, integer) :-
-    integer(Value),
-    !.
-
-type(Value, float) :-
+is_type(Value, float) :-
     float(Value),
     !.
 
-type(Value, list) :-
+is_type(Value, integer) :-
+    integer(Value),
+    !.
+
+is_type(Value, list) :-
     is_list(Value),
     !.
 
-type(Value, string) :-
+is_type(Value, string) :-
     string(Value),
     !.
 
-type(Value, Cname) :-
-    is_class(Cname),
+is_type(Value, Cname) :-
     is_instance(Value),
+    instance(_, Cname, _) = Value,
     !.
 
-type(Value, Cname) :-
-    is_class(Cname),
+is_type(Value, Cname) :-
     inst(Value, Inst),
-    is_instance(Inst).
+    instance(_, Cname, _) = Inst,
+    !.
+
+is_type(Value, Type) :-
+    subtype(SubType, Type),
+    is_type(Value, SubType).
 
 
-subtype(_, any) :- !.
+%%% used to determines types relations
+subtype(SubType, Type) :-
+    is_class(Type),
+    !,
+    superclass(Type, SubType).
 
-subtype(integer, number) :- !.
+subtype(SubType, Type) :-
+    type_chain(SubType, Type).
 
-subtype(float, number) :- !.
+subtype(SubType, Type) :-
+    Type \= any,
+    type_chain(X, Type),
+    subtype(SubType, X).
 
-subtype(Type, Type).
+subtype(SubType, SubType).
+
+%%% defines the subtype relations (one-on-one) 
+type_chain(integer, float).
+
+type_chain(float, number).
+
+type_chain(_, any).
 
 
-contains([El | _], El) :- !.
+contains(El, [El | _]) :- !.
 
-contains([_ | Tail], El) :-
-    contains(Tail, El).
+contains(El, [_ | Tail]) :-
+    contains(El, Tail).
